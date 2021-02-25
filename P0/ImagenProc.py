1901aabede9fbbb259fdcd5477b12d729bbae20a
valdr stiglitz <dark_reggae_93@comunidad.unam.mx>
2021-02-25 14:11:29 -0600

    practica 0

---
+++ P0/ImagenProc.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import io
+from skimage.color import convert_colorspace, rgb2gray
+from PIL import Image
+
+class ImagenProc():
+    def __init__(self, img_file=''):
+        self.img_file = img_file
+        self.image = io.imread(self.img_file)/255.0
+        self.color_space = Image.open(self.img_file).mode
+        self.format = Image.open(self.img_file).format
+
+        
+    def get_datos(self):
+        print("""
+        nombre del archivo: {}
+        tamaño:{}x{}
+        tipo_imagen:{}
+        color:{}
+        """.format(
+            self.img_file, 
+            self.image.shape[0],
+            self.image.shape[1],
+            self.format,
+            self.color_space
+            )
+        )
+    
+    def convert_colorspace(self, dest):
+        try:
+            if(dest != "GRAY"):
+                self.image = convert_colorspace(self.image, self.color_space, dest)
+                self.color_space = dest
+            else:
+                self.image = rgb2gray(self.image)
+            
+        except Exception as e:
+            print(e,"Espacio de color de destino no valido")
+
+    def get_RGB_components(self):
+        image_red = np.copy(self.image)
+        image_red[:,:,1] = 0
+        image_red[:,:,2] = 0
+        image_green = np.copy(self.image)
+        image_green[:,:,0] = 0
+        image_green[:,:,2] = 0
+        image_blue = np.copy(self.image)
+        image_blue[:,:,0] = 0
+        image_blue[:,:,1] = 0
+        return (image_red, image_green, image_blue)
+
+    def crop_image(self, y1,y2, x1,x2):
+        self.image = self.image[y1:y2, x1:x2]
+
+    def show_image(self, show_color_canal = False, title_orig = "Imagen original", show = True, gray = False):
+        plt.figure()
+        plt.title(title_orig)
+        if(gray):
+            plt.imshow(self.image,cmap=plt.cm.gray)
+        else:
+            plt.imshow(self.image)
+        if(show_color_canal):
+            rgb_image = self.get_RGB_components()
+            plt.figure()
+            plt.title("Canal rojo")
+            plt.imshow(rgb_image[0])
+            plt.figure()
+            plt.title("Canal verde")
+            plt.imshow(rgb_image[1])
+            plt.figure()
+            plt.title("Canal azul")
+            plt.imshow(rgb_image[2])
+        if(show):
+            plt.show()
---
+++ P0/__main__.py
@@ -0,0 +1,34 @@
+from ImagenProc import ImagenProc
+if __name__ == '__main__':
+    imp = ImagenProc("imagenes2/magriclonRGB.jpg")
+    
+    imp_retina = ImagenProc("imagenes2/retinaRGB.jpg")
+    imp_retina_orig = ImagenProc("imagenes2/retinaRGB.jpg")
+    imp_corte_bmp = ImagenProc("imagenes2/corte.bmp")
+    '''
+    imp.get_datos()
+    imp.show_image(title_orig="Imagen Original", show_color_canal=True, show=False)
+    
+    imp_retina.get_datos()
+    imp_retina.show_image(title_orig="Imagen Original", show=False)
+    imp_retina.convert_colorspace('GRAY')
+    imp_retina.show_image(title_orig="Imagen escala grises", gray=True, show=False)
+    imp_retina = imp_retina_orig
+    imp_retina.convert_colorspace('HSV')
+    imp_retina.show_image(title_orig="Imagen HSV", show=False)
+    imp_retina = imp_retina_orig
+    imp_retina.convert_colorspace('YUV')
+    imp_retina.show_image(title_orig="Imagen YUV", show=False)
+    imp_retina = imp_retina_orig
+    '''
+
+    imp_corte_bmp.get_datos()
+    imp_corte_bmp.show_image(title_orig="Corte original", show=False)
+    imp_corte_bmp.crop_image(163,502,137,412)
+    imp_corte_bmp.get_datos()
+    imp_corte_bmp.show_image(title_orig="Corte 1/3 de su tamaño", show=False)
+    imp_corte_bmp.crop_image(0, 100, 0,100)
+    imp_corte_bmp.get_datos()
+    imp_corte_bmp.show_image(title_orig="Corte lugar centrico")
+
+    
